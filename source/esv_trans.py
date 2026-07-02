436239db1b3629e418931e209aff2fdedbd6c212
jonathanrobin13 <jonathan.robin13@gmail.com>
2026-07-01 22:50:40 -0400

    made a simple program that got esv verses

--- source/esv_trans.py
+++ source/esv_trans.py
@@ -1,7 +1,39 @@
 import openpyxl
+import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-print(os.getenv("ESV_API_KEY"))
+ESV_API_KEY = os.getenv("ESV_API_KEY")
+
+url = "https://api.esv.org/v3/passage/text/"
+
+headers = {
+    "Authorization": f"Token {ESV_API_KEY}"
+}
+
+params = {
+    "q": "Genesis 1:1;John 3:16",
+    "include-passage-references": False,
+    "include-verse-numbers": False,
+    "include-first-verse-numbers": False,
+    "include-footnotes": False,
+    "include-footnote-body": False,
+    "include-headings": False,
+    "include-short-copyright": False,
+
+
+
+}
+
+response = requests.get(
+    url,
+    headers=headers,
+    params=params
+)
+
+data = response.json()
+
+for verse in data["passages"]:
+    print(verse.strip())
